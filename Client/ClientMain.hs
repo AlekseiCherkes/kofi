2f256950684fd506e680964656d645469dbeb483
NikolaiPohodenko <pohodenko@gmail.com>
2009-10-28 13:55:06 +0200

    Обрабатывается нажатие кнопки на диалоге создания платежного поручения.
    Signed-off-by: NikolaiPohodenko <pohodenko@gmail.com>

--- Client/ClientMain.hs
+++ Client/ClientMain.hs
@@ -1,10 +1,11 @@
 module Main()
-    where
+       where
 
 import Network
 import System.IO
 import Message
 import Graphics.UI.Gtk
+import Graphics.UI.Gtk.Glade
 
 host = "127.0.0.1"
 port = PortNumber 6555
@@ -15,30 +16,32 @@ testTransaction = CommitedTransaction { reason = "test this client server commun
                                       , amount = 100.0
                                       , priority = Urgent
                                       }
-                  
+                                  
 testRequest = CommitTransaction testTransaction
 
 testMsg = Message { unp = 123456789
                   , body = show testRequest
                   , digest = 0
                   }
-          
+                                                          
 testSend = withSocketsDo $ do
   handle <- connectTo host port
   hPrint handle (show testMsg)
   hClose handle
-  
+                                                  
 testLogToConsole = do
   print ("Send transaction: " ++ (show testMsg))
 
 main :: IO ()
 main = do
   initGUI
-  window <- windowNew
-  button <- buttonNew
-  set window [ containerBorderWidth := 10,
-               containerChild := button ]
-  set button [ buttonLabel := "Send transaction" ]
+  dialogXml <- xmlNew "transaction_dialog.glade"
+  let dialogX = case dialogXml of
+        (Just dialogX) -> dialogX
+        Nothing        -> error "cant load the transaction_dialog.glade file."
+
+  window <- xmlGetWidget dialogX castToWindow "transaction_dialog"
+  button <- xmlGetWidget dialogX castToButton "conmmit_btn"
   onClicked button (testSend >> testLogToConsole)
   onDestroy window mainQuit
   widgetShowAll window
