4ee2d38c62fc99154d1f01d7b8dcd2dd731f43cb
NikolaiPohodenko <pohodenko@gmail.com>
2009-11-22 13:26:57 +0200

    Main window is implemented.

--- Client/ClientMain.hs
+++ Client/ClientMain.hs
@@ -15,27 +15,48 @@ host = "127.0.0.1"
 port = PortNumber 6555
 
 
-                                  
-                  
-
-                                                          
+                                                                                       
 testSend message = withSocketsDo $ do
 	handle <- connectTo host port
 	hPrint handle (show message)
 	hClose handle
                                                   
+
+
+makeMessage :: Integer -> Request -> Message
+makeMessage userUnp request = Message { unp = userUnp
+                              , body = show request
+                              , digest = 0
+                              }
+
+    
+onCommitTransactionClicked :: TransactionDialog -> IO ()
+onCommitTransactionClicked gui = do
+    trans <- getTransactionDialogData gui
+    let userUnp = 123456789
+    let msg = makeMessage userUnp (CommitTransaction  trans)
+    (testSend msg) >> (testLogToConsole msg)
+  
+    
+testLogToConsole :: (Show a) => a -> IO ()
 testLogToConsole message= do
   print ("Send transaction: " ++ (show message))
   
   
 
+bindActions :: ActionGroup -> IO ()
+bindActions actions = do
+    (Just payAction) <- actionGroupGetAction actions "NewPay_a"
+    onActionActivate payAction (putStrLn "Transaction dialog launch!")--(onNewTransaction onCommitTransactionClicked)
+    return ()
+
   
 
 main :: IO ()
 main = do
-	initGUI
-  
-	gui <- loadMainWindow "mainWindow.glade"
-	onDestroy (window gui) mainQuit
-	widgetShowAll (window gui)
-	mainGUI
+    initGUI
+    gui <- loadMainWindow
+    bindActions   (actions gui)
+    onDestroy     (window  gui) mainQuit
+    widgetShowAll (window  gui)
+    mainGUI
