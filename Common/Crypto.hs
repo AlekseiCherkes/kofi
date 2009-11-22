e9d7f3aa3bc8be591482b6b8c9debed92b9c3a75
AlekseiCherkes <aleksei.cherkes@gmail.com>
2009-11-22 14:29:34 +0200

    Crypto module works.

--- Common/Crypto.hs
+++ Common/Crypto.hs
@@ -5,6 +5,7 @@ import Debug.Trace
 
 import Message
 import Data.Word
+import Data.List
 import Codec.Utils
 import qualified Data.Digest.MD5 as MD5
 import qualified Codec.Encryption.RSA as RSA
@@ -17,35 +18,54 @@ import qualified Codec.Binary.Base64.String as B64
 type RSAKey = (Base64, Base64) -- (n, e)
 type EncryptedMessage = Base64
 
--- createMessage :: RSAKey -> SenderId -> String -> Message
--- createMessage key senderId text = Message { senderId = senderId, 
---                                             digest = digest,
---                                             text = text }
---   where digest = B64.encode $ toChars $ MD5.hash $ toOctetsMy text
-
-encodeMessage :: RSAKey -> String -> EncryptedMessage
-encodeMessage key msg = (--B64.encode . 
-                         octetsToChar8 . 
-                         groupMap 1 (RSA.encrypt dkey) . 
-                         char16ToOctets) msg
-  where dkey = b64ToKey key
-        keyLen = length $ fst dkey
-
-decodeMessage :: RSAKey -> EncryptedMessage -> String -- Message
-decodeMessage key msg = dmsg -- read dmsg
-  where dmsg = (octetsToChar16 .
-                --groupMap 2 (RSA.decrypt dkey) .
-                char8ToOctets)
-                --B64.decode) 
-               msg
+createMessage :: RSAKey -> SenderId -> String -> Message
+createMessage key senderId text = Message { senderId = senderId, 
+                                            digest = digest,
+                                            text = text }
+  where digest = ( B64.encode .
+                   octetsToChar8 .
+                   groupMap (keyLen - 1) (RSA.encrypt dkey) .
+                   getHash ) text
         dkey = b64ToKey key
         keyLen = length $ fst dkey
 
--- verifyMessage :: Message -> RSAKey -> Bool
+verifyMessage :: RSAKey -> Message -> Bool
+verifyMessage key msg = isPrefixOf ourDigest theirsDigest
+  where ourDigest = getHash (text msg)
+        theirsDigest = ( groupMap (keyLen) decodeChunk .
+                         char8ToOctets .
+                         B64.decode ) (digest msg)
+          where 
+            dkey = b64ToKey key
+            keyLen = length $ fst dkey
+            decodeChunk = take (keyLen - 1) . reverse . RSA.decrypt dkey
+                
+
+encodeMessage :: RSAKey -> Message -> EncryptedMessage
+encodeMessage key msg = (B64.encode . 
+                         octetsToChar8 . 
+                         groupMap (keyLen - 1) (RSA.encrypt dkey) . 
+                         char16ToOctets) (show msg)
+  where dkey = b64ToKey key
+        keyLen = length $ fst dkey
+
+decodeMessage :: RSAKey -> EncryptedMessage -> Message
+decodeMessage key msg = read dmsg
+  where dmsg = (octetsToChar16 .
+                groupMap keyLen decodeChunk .
+                char8ToOctets .
+                B64.decode) 
+               msg
+        dkey = b64ToKey key
+        keyLen = length $ fst dkey
+        decodeChunk = take (keyLen - 1) . reverse . RSA.decrypt dkey
 
 --------------------------------------------------------------------------------
 -- Utilitis
 --------------------------------------------------------------------------------
+
+getHash :: String  -> [Octet]
+getHash str = (MD5.hash . char16ToOctets) str
 
 groupMap n f xs | n == 0 = error "groupMap: n == 0"
                 | length xs >= n = f (take n xs) ++ groupMap n f (drop n xs)
@@ -65,7 +85,7 @@ char16ToOctets cs = listToOctets $ map charToWord cs
                                              
 -- !!!
 octetsToChar16 :: [Octet] -> String
-octetsToChar16 os = map wordToChar (listFromOctets os)
+octetsToChar16 os = map wordToChar (listFromOctets os :: [Word16])
   where wordToChar = (toEnum . fromEnum) :: Word16 -> Char
         
 keyToB64 :: ([Octet], [Octet]) -> RSAKey
