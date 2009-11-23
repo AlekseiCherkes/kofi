f233e0c82cdebc87957c7b4a19d07984c0a73a03
NikolaiPohodenko <pohodenko@gmail.com>
2009-11-23 18:58:35 +0200

    Client stabilization after merge.

--- Client/Main.hs
+++ Client/Main.hs
@@ -9,52 +9,52 @@ import Graphics.UI.Gtk.Glade ()
 
 import Message
 import TransactionDialog
+import MainWindow
 
 host = "127.0.0.1"
 port = PortNumber 6555
 
-testTransaction = CommitedTransaction { reason = "test this client server communication"
-                                      , creditAccountId = 123456789
-                                      , debitAccountId = 987654321
-                                      , amount = 100.0
-                                      , priority = Urgent
-                                      }
-                                  
-                  
-makeMessage :: Request -> Message
-makeMessage request = Message { unp = 123456789
-                              , body = show request
-                              , digest = 0
-                              }
-                                                          
+
+
 testSend message = withSocketsDo $ do
-  handle <- connectTo host port
-  hPrint handle (show message)
-  hClose handle
+    handle <- connectTo host port
+    hPrint handle (show message)
+    hClose handle
                                                   
+
+    
+testLogToConsole :: (Show a) => a -> IO ()
 testLogToConsole message= do
   print ("Send transaction: " ++ (show message))
   
   
-onCommitTransactionClicked :: TransactionDialog -> IO ()
-onCommitTransactionClicked gui = do
-  trans <- getTransactionDialogData gui
-  let msg = makeMessage $ CommitTransaction trans
-  (testSend msg) >> (testLogToConsole msg)
+  
+makeMessage :: String -> Request -> Message
+makeMessage unp request = Message { senderId = ClientId unp
+                                  , text = show request
+                                  , digest = "0"
+                                  }
+
+  
+onCommitTransactionClicked :: CommitedTransaction -> IO ()
+onCommitTransactionClicked trans = do
+    let msg = makeMessage "123456789" (CommitTransaction trans)
+    (testSend msg) >> (testLogToConsole msg)
+  
   
 
+bindActions :: ActionGroup -> IO ()
+bindActions actions = do
+    (Just payAction) <- actionGroupGetAction actions "NewPay_a"
+    onActionActivate payAction (onNewTransaction onCommitTransactionClicked)
+    return ()
+
+  
 main :: IO ()
 main = do
-  initGUI
-  
-  gui <- loadTransactionDialog "transaction_dialog.glade"
-  setTransactionDialogData gui testTransaction
-  
-  trans <- getTransactionDialogData gui
-  let msg = makeMessage $ CommitTransaction trans
-  
-  onClicked (commit_btn gui) $ onCommitTransactionClicked gui
-  onDestroy (dialog_wnd gui) mainQuit
-  onDestroy (cancel_btn gui) mainQuit
-  widgetShowAll (dialog_wnd gui)
-  mainGUI
+    initGUI
+    gui <- loadMainWindow
+    bindActions   (actions gui)
+    onDestroy     (window  gui) mainQuit
+    widgetShowAll (window  gui)
+    mainGUI
