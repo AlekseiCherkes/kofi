731caef51c3e521222b0232292a82be6d2b6897f
AlekseiCherkes <aleksei.cherkes@gmail.com>
2009-11-29 14:31:12 +0200

    Logger functionaity improved: logs are placed in separate dir.

--- Src/Server/Loggers.hs
+++ Src/Server/Loggers.hs
@@ -2,6 +2,7 @@ module Loggers(withLoggers)
        where
 
 import System.IO
+import System.Directory
 import Control.Exception
 import System.Log.Logger
 import System.Log.Handler.Simple
@@ -9,13 +10,17 @@ import qualified System.Log.Handler
 
 withLoggers = bracket acquireLoggers releaseLoggers
 
-acquireLoggers = do 
+logDir = "log/"
+
+acquireLoggers = do
   stderrH <- verboseStreamHandler stderr INFO
-  -- stdoutH <- verboseStreamHandler stdout DEBUG
-  h <- fileHandler "server.log" DEBUG
-  dbH <- fileHandler "server.db.log" DEBUG
-  serverH <- fileHandler "server.server.log" DEBUG
-  clientsH <- fileHandler "server.clients.log" DEBUG
+  
+  createDirectoryIfMissing True logDir
+  let logFileHandler path = fileHandler (logDir ++ path)
+  h <- logFileHandler "server.log" DEBUG
+  dbH <- logFileHandler "server.db.log" DEBUG
+  serverH <- logFileHandler "server.server.log" DEBUG
+  clientsH <- logFileHandler "server.clients.log" DEBUG
   
   updateGlobalLogger (rootLoggerName::String) (setLevel DEBUG . setHandlers [stderrH])
   updateGlobalLogger "server" (setLevel DEBUG . setHandlers [h])
