a36ab7b70a7485c1a484d6cbbd356cf72b92fbcd
Vadim Shpakovsky <shpak.vadim@gmail.com>
2009-12-13 14:36:36 +0200

    Some database corrections.

--- Bin/Client/create.sql
+++ Bin/Client/create.sql
@@ -1,32 +1,32 @@
-CREATE TABLE Company (
+create table Company (
 	company_unp char(13) not null,
 	company_name varchar(256) not null,
 	primary key (company_unp)
 );
 		
-CREATE TABLE CounterParty (
+create table CounterParty (
 	payer_unp char(13) not null,
 	beneficiary_unp char(13) not null,
 	primary key (payer_unp, beneficiary_unp),
 	foreign key (payer_unp)
-		references Company(company_unp),
+		references company(company_unp),
 	foreign key (beneficiary_unp)
-		references Company(company_unp)
+		references company(company_unp)
 );
 
-CREATE TABLE Bank (
+create table Bank (
 	bank_bic char(9) not null,
 	bank_name varchar(256) not null,
 	primary key (bank_bic)
 );
 
-CREATE TABLE Account (
+create table Account (
 	acc_id int not null,
 	company_unp char(13) not null,
 	bank_bic char(9) not null,
 	primary key (acc_id, company_unp, bank_bic),
 	foreign key (company_unp)
-		references Company(company_unp),
+		references company(company_unp),
 	foreign key (bank_bic)
-		references Bank(bank_bic)
+		references bank(bank_bic)
 );
--- Bin/Server/create.sql
+++ Bin/Server/create.sql
@@ -1,4 +1,4 @@
-CREATE TABLE Account (
+create table Account (
 	account_id char(13) not null,
 	bank_id char(9) not null,
 	owner_id char(13) not null,
@@ -6,50 +6,50 @@ CREATE TABLE Account (
 	open_date timestamp not null,
 	close_date timestamp null,
 	primary key (account_id, bank_id),
-	FOREIGN KEY(owner_id)
-		REFERENCES Client(client_unp)
-		ON DELETE CASCADE
-		ON UPDATE CASCADE
+	foreign key(owner_id)
+		references client(client_unp)
+		on delete cascade
+		on update cascade
 );
 					  
-CREATE TABLE Client (
-	client_unp char(13) not null,
+create table Company (
+	company_unp char(13) not null,
 	name text  not null,
 	registry_date timestamp  not null,
 	unregistry_date timestamp  null,
-	open_key varchar(200)  not null,
+	open_key varchar(1024)  not null,
 	primary key (client_unp)
 );
 					  
-CREATE TABLE CommitedTransaction (
+create table CommitedTransaction (
 	transaction_id int not null,
 	commit_date timestamp not null,
 	recive_date timestamp not null,
 	status_id int not null,
 	content text not null,
 	reason text not null,
-	payer_account_id varchar(9) not null,
-	payer_bank_id varchar(9) not null,
+	payer_account_id char(13) not null,
+	payer_bank_id char(9) not null,
 	payer_final_balance double precision not null,
-	beneficiary_account_id varchar(9) not null,
-	beneficiary_bank_id varchar(9) not null,
+	beneficiary_account_id char(13) not null,
+	beneficiary_bank_id char(9) not null,
 	beneficiary_final_balance double precision not null,
 	amount double precision not null,
 	priority int not null,
 	primary key (transaction_id),
-	FOREIGN KEY(payer_account_id)
-		REFERENCES Account(account_id),
-	FOREIGN KEY(payer_bank_id)
-		REFERENCES Account(bank_id),
-	FOREIGN KEY(beneficiary_account_id)
-		REFERENCES Account(account_id),
-	FOREIGN KEY(beneficiary_bank_id)
-		REFERENCES Account(bank_id),
-	FOREIGN KEY(status_id)
-		REFERENCES Status(status_id)
+	foreign key(payer_account_id)
+		references account(account_id),
+	foreign key(payer_bank_id)
+		references account(bank_id),
+	foreign key(beneficiary_account_id)
+		references account(account_id),
+	foreign key(beneficiary_bank_id)
+		references account(bank_id),
+	foreign key(status_id)
+		references status(status_id)
 );
 
-CREATE TABLE Status (
+create table Status (
 	status_id int primary key,
 	message text not null
 );
