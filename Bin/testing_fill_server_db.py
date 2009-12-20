4bd3f3ce61daf06ba7a1e0c1668f566a14434a8f
AlekseiCherkes <aleksei.cherkes@gmail.com>
2009-12-20 10:25:11 +0200

    Bin/ make infrustructure improved.

---
+++ Bin/testing_fill_server_db.py
@@ -0,0 +1,7 @@
+#-*- coding: utf-8 -*-
+
+# Заполняется база данных сервера последовательным вызовом утилит create_company, create_account
+# Таблица СommitedTransactions заполняется insert-ами.
+# Таблица statuses заполняется при созданжии серверной базы  (сreate.sql). Здесь заполнять не нужно.
+
+print "Im testing_fill_server_db.py"
---
+++ Bin/testing_generate_client_configs.py
@@ -0,0 +1,5 @@
+#-*- coding: utf-8 -*-
+
+# Cгенерировать несколько конфигов для клиента, на основе информации занесённой в Server/server.db
+
+print "Im testing_generate_client_configs.py"
