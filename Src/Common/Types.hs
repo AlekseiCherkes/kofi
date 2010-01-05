c45225dafdb9680de0a87f5a4f235a922d8b1498
AlekseiCherkes <aleksei.cherkes@gmail.com>
2010-01-05 19:49:54 +0200

    Eq implementation added to some types.

--- Src/Common/Types.hs
+++ Src/Common/Types.hs
@@ -1,6 +1,8 @@
 module Types 
        where
 
+import Data.List
+
 import Validation
 
 type Name = String
@@ -8,6 +10,23 @@ type Name = String
 data UNP  = C_UNP  { unp2str :: String} deriving (Read, Show)
 data BIC  = C_BIC  { bic2str :: String} deriving (Read, Show)
 data ACC  = C_ACC  { acc2str :: String} deriving (Read, Show)
+
+instance Eq UNP where 
+  (==) op1 op2 = isPrefixOf s1 s2 && isSuffixOf s1 s2
+    where s1 = unp2str op1
+          s2 = unp2str op2
+          
+instance Eq BIC where 
+  (==) op1 op2 = isPrefixOf s1 s2 && isSuffixOf s1 s2
+    where s1 = bic2str op1
+          s2 = bic2str op2
+
+
+instance Eq ACC where 
+  (==) op1 op2 = isPrefixOf s1 s2 && isSuffixOf s1 s2
+    where s1 = acc2str op1
+          s2 = acc2str op2
+
 
 str2bic :: String -> BIC
 str2bic str = if (isValidBic str) then (C_BIC str) else error "Invalid BIC."
