2a77d5c73106c9349ea5908378f69417ec626f49
AlekseiCherkes <aleksei.cherkes@gmail.com>
2010-01-06 10:22:11 +0200

    Types.hs formatted more nicelly.

--- Src/Common/Types.hs
+++ Src/Common/Types.hs
@@ -1,32 +1,29 @@
 module Types 
        where
 
-import Data.List
-
 import Validation
 
+--------------------------------------------------------------------------------
+-- Types
+--------------------------------------------------------------------------------
+
 type Name = String
+type Base64 = String
+type RSAKey = (Base64, Base64)
+type MessageBody = String
+type EncryptedMessageBody = Base64
 
-data UNP  = C_UNP  { unp2str :: String} deriving (Read, Show)
-data BIC  = C_BIC  { bic2str :: String} deriving (Read, Show)
-data ACC  = C_ACC  { acc2str :: String} deriving (Read, Show)
+data UNP  = C_UNP  { unp2str :: String} deriving (Read, Show, Eq)
+data BIC  = C_BIC  { bic2str :: String} deriving (Read, Show, Eq)
+data ACC  = C_ACC  { acc2str :: String} deriving (Read, Show, Eq)
 
-instance Eq UNP where 
-  (==) op1 op2 = isPrefixOf s1 s2 && isSuffixOf s1 s2
-    where s1 = unp2str op1
-          s2 = unp2str op2
-          
-instance Eq BIC where 
-  (==) op1 op2 = isPrefixOf s1 s2 && isSuffixOf s1 s2
-    where s1 = bic2str op1
-          s2 = bic2str op2
+data AccountPK = AccountPK { accId   :: ACC
+                           , bankBic :: BIC }
+                deriving (Read, Show)
 
-
-instance Eq ACC where 
-  (==) op1 op2 = isPrefixOf s1 s2 && isSuffixOf s1 s2
-    where s1 = acc2str op1
-          s2 = acc2str op2
-
+--------------------------------------------------------------------------------
+-- Conversion
+--------------------------------------------------------------------------------
 
 str2bic :: String -> BIC
 str2bic str = if (isValidBic str) then (C_BIC str) else error "Invalid BIC."
@@ -37,14 +34,6 @@ str2unp str = if (isValidUnp str)  then (C_UNP str) else error "Invalid UNP."
 str2acc :: String -> ACC
 str2acc str = if (isValidAcc str) then (C_ACC str) else error "Invalid BIC."
                                         
-
-data AccountPK = AccountPK { accId   :: ACC
-                           , bankBic :: BIC }
-                deriving (Read, Show)
-
-type Base64               = String
-type RSAKey               = (Base64, Base64)
-type MessageBody          = String
-type EncryptedMessageBody = Base64
-
-
+--------------------------------------------------------------------------------
+-- End
+--------------------------------------------------------------------------------
