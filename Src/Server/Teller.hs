3885087fb5a80c56f627955eb1f82d0bdf5f2acc
AlekseiCherkes <aleksei.cherkes@gmail.com>
2010-01-06 21:49:27 +0200

    Teller validation significantly improved.

--- Src/Server/Teller.hs
+++ Src/Server/Teller.hs
@@ -28,7 +28,7 @@ errorM = Logger.errorM "root.teller"
 -- Types
 --------------------------------------------------------------------------------
 
-data Transaction = Transaction { transactionUnp :: UNP
+data Transaction = Transaction { transactionUNP :: UNP
                                , transactionPayerAccPK :: AccountPK
                                , transactionBnfcAccPK :: AccountPK
                                , transactionAmount :: Double
@@ -44,16 +44,18 @@ data Transaction = Transaction { transactionUnp :: UNP
 --------------------------------------------------------------------------------
 
 -- 0 "Success."
--- 1 "Invalid payer's BIC."
--- 2 "Invalid beneficiary's BIC."
--- 3 "Invalid payer's account."
--- 4 "Invalid beneficiary's account."
--- 5 "Payer hasn't this account."
--- 6 "Payer is closed already."
--- 7 "Beneficiary is closed already."
--- 8 "Payer's account is closed already."
--- 9 "Beneficiary's account is closed already."
--- 10 "Payer hasn't enough money for commited this transaction."
+-- 1 "System error.
+-- 2 "Invalid payer's BIC."
+-- 3 "Invalid beneficiary's BIC."
+-- 4 "Invalid payer's account."
+-- 5 "Invalid beneficiary's account."
+-- 6 "Payer hasn't this account."
+-- 7 "Payer is closed already."
+-- 8 "Beneficiary is closed already."
+-- 9 "Payer's account is closed already."
+-- 10 "Beneficiary's account is closed already."
+-- 11 "Coincidence of payer and beneficiary's accounts."
+-- 12 "Payer hasn't enough money for commited this transaction."
 
 type ChekerMonad = ErrorT Int IO (Double, Double)
 
@@ -79,21 +81,61 @@ retriveAcc err apk = do
     else do
     liftIO $ errorM $ "Can't retrive account by apk = " ++ (show apk)
     throwError err
-                         
+
+retriveCmp err unp = do
+  cmp <- liftIO $ DM.findCompanyByUNP unp
+  if (isJust cmp)
+    then do
+    liftIO $ infoM $ "Company retrived: " ++ (show $ fromJust cmp)
+    return $ fromJust cmp
+    else do
+    liftIO $ errorM $ "Can't retrive company by unp = " ++ (show unp)
+    throwError err
+
+checkDate err field = do
+  if (isNothing $ field)
+    then do
+    liftIO $ infoM $ "Entity is opened."
+    else do
+    liftIO $ errorM $ "Entity closed. Close date: " ++ (show field)
+    throwError err
+
+checkAccountOwner err acc cmp = do
+  if ((DM.accountOwnerUnp acc) == (DM.companyUnp cmp)) 
+    then do
+    liftIO $ infoM "Account belongs to author of the reqest."
+    else do
+    liftIO $ errorM $ "Author of the request hasn't have responsed account."
+    throwError err
+
 checkTransaction :: Transaction -> ChekerMonad
 checkTransaction t = do
-  pb <- retriveBank 1 $ bankBic $ transactionPayerAccPK t
-  bb <- retriveBank 2 $ bankBic $ transactionPayerAccPK t
-  pa <- retriveAcc 3 $ transactionPayerAccPK t
-  ba <- retriveAcc 4 $ transactionBnfcAccPK t
-  
+  pb <- retriveBank 2 $ bankBic $ transactionPayerAccPK t
+  bb <- retriveBank 3 $ bankBic $ transactionPayerAccPK t
+  pa <- retriveAcc 4 $ transactionPayerAccPK t
+  ba <- retriveAcc 5 $ transactionBnfcAccPK t
+
+  pc <- retriveCmp 1 $ DM.accountOwnerUnp pa
+  bc <- retriveCmp 1 $ DM.accountOwnerUnp ba
+
+  checkAccountOwner 6 pa pc
+
+  checkDate 7 $ DM.companyUnregistryDate pc
+  checkDate 8 $ DM.companyUnregistryDate bc  
+  checkDate  9 $ DM.accountCloseDate pa 
+  checkDate 10 $ DM.accountCloseDate ba
+
+  if (DM.accountPK pa) == (DM.accountPK ba)
+    then throwError 11
+    else liftIO $ return ()
+
   let pab = DM.accountBallance pa
   let bab = DM.accountBallance ba
   let delta = transactionAmount t
 
-  if pab > delta 
+  if pab >= delta 
     then return (pab - delta, bab + delta )
-    else throwError 10
+    else throwError 12
 
 --------------------------------------------------------------------------------
 -- Functions
@@ -107,10 +149,10 @@ processTransaction tt = do
   (status, payerBallance, bnfcBallance) <-
     case r of
       Right (p, b) -> do
-        infoM "Transaction checked. Everething is OK." 
+        infoM $ "Transaction checked. Everething is OK." 
         return $ (0, Just p, Just b)
       Left s -> do
-        errorM "Transaction checked. Thomething is BAD."  
+        errorM $ "Transaction checked. Thomething is BAD. Error code: " ++ (show s)
         return $ (s, Nothing, Nothing)
 
   dummyDate <- toCalendarTime $ TOD 0 0
@@ -121,11 +163,13 @@ processTransaction tt = do
             (transactionPayerAccPK tt) (transactionBnfcAccPK tt)
             (payerBallance) (bnfcBallance)
             (transactionAmount tt) (transactionPriority tt)
-        
+
   DM.insertTransaction dmt
-  
-  if (isJust payerBallance) && (isJust bnfcBallance) 
-    then DM.updateAccountBallance (transactionPayerAccPK tt) $ fromJust payerBallance
+
+  if (isJust payerBallance) && (isJust bnfcBallance)
+    then do
+    DM.updateAccountBallance (transactionPayerAccPK tt) $ fromJust payerBallance
+    DM.updateAccountBallance (transactionBnfcAccPK tt) $ fromJust bnfcBallance
     else return ()
 
 startTeller transactionChannel channelName delay = forever $ do
